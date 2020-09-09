3b60ce239ba606f02296baacae282c10a6502bd5
Aarush Goyal <64161383+Aarush-Goyal@users.noreply.github.com>
2020-09-09 22:26:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
+// TODO: Most Important
+// Never Update any variable from the state directly
+
+function App(){
+const [count, setCount] = useState(0)
+return(
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <h1>State Counter App</h1>
       </header>
+      <h2>Count = {count}</h2>
+      <button onClick={()=>{setCount(0)}}>Reset</button>
+      <div>
+        <button onClick={()=>(count >= 10 ? "":setCount(count + 1))}>Increase</button>
+        <button onClick={()=>(count === 0 ? "":setCount(count -1))}>Reduce</button>
+      </div>
+      <div>
+        <h3>Some Info About the App</h3>
+        <p>The app uses the concept of state to Increase and Decrease the Value of count</p>
+      </div>
+      <p><b>Note:</b> The app only counts till 10</p>
     </div>
   );
 }
-
 export default App;
